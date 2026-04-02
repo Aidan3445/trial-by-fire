480d4992827b85a85998f831f3e1e1822066cb37
Aidan Weinberg <aidanhweinberg@gmail.com>
2026-04-02 13:09:11 -0500

    Update ci_post_clone.sh
    Duh

--- apps/mobile/ios/ci_scripts/ci_post_clone.sh
+++ apps/mobile/ios/ci_scripts/ci_post_clone.sh
@@ -7,7 +7,8 @@ brew install node cocoapods
 cd "$CI_PRIMARY_REPOSITORY_PATH"
 echo "Installing node modules..."
 npm ci
-cd ios
+pwd
+cd apps/mobile/ios
 echo "Setting up CocoaPods CDN..."
 pod repo add-cdn trunk https://cdn.cocoapods.org/
 echo "Installing pods..."
