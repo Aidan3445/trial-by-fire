eef749071c41c09d7b9f2323c83004f7d5945781
Aidan Weinberg <aidanhweinberg@gmail.com>
2026-08-29 09:03:51 -0700

    Disable SeasonOverNotice mobile

--- apps/mobile/src/app/(protected)/_layout.tsx
+++ apps/mobile/src/app/(protected)/_layout.tsx
@@ -9,7 +9,7 @@ import { Asset } from 'expo-asset';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useChangeLeaderboardUsername } from '~/hooks/livePredictions/mutation/useChangeLeaderboardUsername';
 import { useAuth } from '@clerk/expo';
-import SeasonOverNotice from '~/components/sys/seasonOverNotice';
+//import SeasonOverNotice from '~/components/sys/seasonOverNotice';
 
 export const FIRST_LOGIN_KEY = 'hasEstablishedLeaderboardUsername';
 const LogoImage = require('~/assets/LogoFull.png');
@@ -66,7 +66,6 @@ export default function ProtectedLayout() {
       <QueryClientContextProvider>
         <NotificationManager />
         <FirstLoginManager />
-        <SeasonOverNotice />
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
           <Stack.Screen
